fdc358b5a0bacd8018ccedfae5d7fc79192e0af5
mutasem <motasemimad10@gmail.com>
2021-10-19 15:41:37 +0300

    starting the build

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBqrdCBC7KZbK25FkrSWHoDk_q9ol2K4is',
+  authDomain: 'slack-clone-f0aa6.firebaseapp.com',
+  projectId: 'slack-clone-f0aa6',
+  storageBucket: 'slack-clone-f0aa6.appspot.com',
+  messagingSenderId: '229769536684',
+  appId: '1:229769536684:web:be21061ca8ade06c93ab09',
+};
