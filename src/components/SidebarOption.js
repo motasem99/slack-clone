23be5493071d68e4c5fb7b988e8c943be3d9f000
mutasem <motasemimad10@gmail.com>
2021-10-23 18:32:17 +0300

    using redux for sidebar

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
 import { db } from '../firebase';
+import { enterRoom } from '../features/appSlice';
 
 const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+  const dispatch = useDispatch();
+
   const addChannel = () => {
     const channelName = prompt('Please enter the channel name');
 
@@ -13,7 +17,15 @@ const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
     }
   };
 
-  const selectChannel = () => {};
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
 
   return (
     <SidebarOptionContainer
